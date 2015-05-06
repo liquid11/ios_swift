4146ac970a2879fc029b1c6ea630583e898311f2
Adeel Zaidi <adeel.zaidi@tezrosolutions.com>
2015-05-06 20:47:28 +0500

    Initial Commit

---
+++ iossample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iossample
+//
+//  Created by Tezro Solutions on 06/05/2015.
+//  Copyright (c) 2015 Tezro Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
