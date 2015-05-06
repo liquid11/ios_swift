b6c20d407bbbfc909356b76acf11dffcf4567a2d
Adeel Zaidi <adeel.zaidi@tezrosolutions.com>
2015-05-06 21:55:29 +0500

    ....

--- iossample/ViewController.swift
+++ iossample/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtId: UITextField!
+    @IBOutlet var txtPass: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,8 +21,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+   }
+    
+    @IBAction func btnLogin(sender: UIButton) {
+        
+    self.txtId.resignFirstResponder()
+        
     }
-
-
 }
 
